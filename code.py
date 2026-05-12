872e04dba276401b45c24ac115ad3af61f978ebd
Tu Nguyen <86095555+BobbyNguyen2903@users.noreply.github.com>
2026-05-12 09:20:35 +0700

    Update code.py

--- code.py
+++ code.py
@@ -1,26 +1,25 @@
-from smbus2 import SMBus, i2c_msg 
-import time 
-DEVICE_ADDRESS = 0x50 
+from smbus2 import SMBus, i2c_msg
+import time
+
+DEVICE_ADDRESS = 0x50
 
 def write_byte(mem_addr, value):
-    with SMBus(1) as bus: 
-        data = [mem_addr >> 8, mem_addr & 0xFF, value]
-        msg = i2c_msg.write(DEVICE_ADDRESS, data) 
-        bus.i2c_rdwr(msg) 
-        time.sleep(0.01) 
+    with SMBus(1) as bus:
+        data = [mem_addr & 0xFF, value]          
+        msg = i2c_msg.write(DEVICE_ADDRESS, data)
+        bus.i2c_rdwr(msg)
+        time.sleep(0.01)
 
 def read_byte(mem_addr):
     with SMBus(1) as bus:
-        write = i2c_msg.write(DEVICE_ADDRESS, [mem_addr >> 8, mem_addr & 0xFF])
-        read = i2c_msg.read(DEVICE_ADDRESS, 1)
+        write = i2c_msg.write(DEVICE_ADDRESS, [mem_addr & 0xFF]) 
+        read  = i2c_msg.read(DEVICE_ADDRESS, 1)
         bus.i2c_rdwr(write, read)
         return list(read)[0]
 
-try:
-    addr = 0x0010 
-    val = 88      
-    write_byte(addr, val) 
-    result = read_byte(addr) 
-    print(f"Kết quả: {result}")
-except Exception as e:
-    print(f"Lỗi rồi: {e}") 
+# Ghi
+write_byte(DEVICE_ADDRESS, 29)
+
+# Đọc
+result = read_byte(DEVICE_ADDRESS)
+print(result)  
